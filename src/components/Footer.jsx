ad4e7a6d2cfe555046b4e7539fd45712c6a3a263
Pablo Fabricio Gaspar <pfg2025ia@gmail.com>
2026-05-10 23:21:04 -0300

    agregué los componentes header, nav, footer y layout.css

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return(
+        <footer>
+            <p><strong>Gestion de Proyectos Educativos</strong></p>
+            <p>Grupo 16</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+const Nav = () => {
+    return(
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Proyectos</a></li>
+                <li><a href="#">Perfil de Usuario</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
