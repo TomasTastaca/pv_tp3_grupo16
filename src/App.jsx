2f078ceccfbb26820bb9b666af3f7aa6d8c5f7f7
Brenda Galan <brendagalan4@gmail.com>
2026-06-06 22:33:09 -0300

    Actualicé las rutas en App

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import './css/styles.css';
 import './css/listaProyectos.css';
 import './css/layout.css';
 import './css/registroActividad.css';
-import ListaProyectos from './components/ListaProyectos';
+import ListaProyectos from './views/ListaProyectos';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 //nuevos import para ruta 
 import { Route, Routes, BrowserRouter as Router, Navigate } from 'react-router-dom';
-import DetalleProyecto from './components/DetalleProyecto';
-/*import DashBoard  from './views/DashBoard';*/
-//import PerfilUsuario from './views/PerfilUsuario';//
+import DetalleProyecto from './views/DetalleProyecto';
+import Dashboard  from './views/Dashboard';
+import PerfilUsuario from './views/PerfilUsuario';
 
 
 const App = () => {
@@ -22,7 +22,7 @@ const App = () => {
       <main>  
         <Routes>
           <Route path="/" element={<Navigate to="/Inicio"/>}/>
-          <Route path="/Inicio" element={<DashBoard/>}/>
+          <Route path="/Inicio" element={<Dashboard/>}/>
           <Route path="/Proyectos" element={<ListaProyectos/>}/>
           {/*"ruta dinamica" para ver los distinto de tipos de proyectos, en este caso tenemos 5*/}
           <Route path="/Proyectos/:id" element={<DetalleProyecto/>}/>
